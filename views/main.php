6484be5a9bba662f1feacb89d562f719890790c3
Anna Hafþórsdóttir <annahaff@gmail.com>
2014-11-20 01:11:53 +0000

    skrifaði texta

--- views/main.php
+++ views/main.php
@@ -8,17 +8,26 @@
 	<h2>Velkomin</h2>
 	<p>Þú hefur rambað inn á síðu sem var smíðuð af Önnu H og Freydísi H. Við erum tvær kennkyns mannverur sem stundum nám í tölvunarfræði. 
 		Ekki nóg með það að við séum brjálæðislega gáfaðar, heldur erum við líka mjög myndarlegar (sjá mynd til hliðar) 
-		og svo erum við ágætlega skemmtilegar líka. Endilega svarið skoðanakönnuninni hér til hliðar og plís skrifið í gestabókina.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		og svo erum við ágætlega skemmtilegar líka. Okkur finnst gaman að fara í bíó, borða pizzu, klappa dýrum, ferðast um heiminn, skoða kattarvideo,
+		 safna hlutum, syngja, dansa, borða nammi og margt margt fleira</p>
+	<p>Á þessari síðu er hægt að skrá sig á póstlista. Við kunnum virkilega vel að meta þá sem skrá sig á póstlistann og munu þeir aðilar fá
+		persónuleg handskrifuð bréf frá okkur einu sinni í viku. Einning munum við draga allháa upphæð af kreditkortinu þínu í hvert sinn sem
+		þú verslar við Samkaup.</p>
+	<p>Ef þér leiðist getur þú tekið þér pásu frá grámylgaða hversdagsleikanum og spilað pacman. Við gerðum þennan pacman leik sem finna má á þessari síðu. Hann virkar eins og 
+		venjulegur pacmanleikur á að virka. Nema stundum virkar hann aðeins öðruvísi</p>
+	<p>Ef þú veist ekki hvað þú átt að gera við þig á kvöldin eða til dæmis ef þú átt enga vini, þá er gráupplagt að kíkja á hvaða tónleikar eru í boði.
+		Það er einning hægt að gera inn á þessari síðu. Það er gott fyrir sál og líkama að dansa við fallega tóna og hver veit, kannski kynnist þú
+		einhverjum nýjum vinum. Ef svo er, endilega segðu þeim frá þessari síðu.</p>
+	<p>Eins og lesa má (eða heyra má ef þú ert ólæs og einhver er að lesa þetta upp fyrir þig), er hægt að gera ýmislegt inn á þessari síðu. Við fyrstu sýn
+		virðist þetta ef til vill vera hálf samhengislaust. En ef betur er að gáð, á þetta allt saman nokkuð marga hluti sameiginlega. Til dæmis eru 
+		þeir allir skemmtilegir. Kettirnir á bannernum eru kettir sem við, Anna og Freydís, höfum ættleitt. Þess vegna eru þeir á bannernum.
+	</p>
+	<p>Plís skrifaðu í gestabókina.</p>
+	
+	<p>Takk</p>
+
 </div>
 
 <div class="pic">
 	<h3>Mynd af okkur</h3>
-</div>
-
-<div class="poll">
-	<h3>Skoðanakönnun</h3>
 </div>
